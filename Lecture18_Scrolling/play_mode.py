d81342b46e33b89a71dead347dfef563990676af
Gwangmin Song <thdalsrhkd@naver.com>
2025-12-02 17:39:43 +0900

    ball 하나 생성

--- Lecture18_Scrolling/play_mode.py
+++ Lecture18_Scrolling/play_mode.py
@@ -9,6 +9,7 @@ import common
 
 from boy import Boy
 from court import Court
+from ball import Ball
 
 
 def handle_events():
@@ -30,6 +31,8 @@ def init():
     common.boy = Boy()
     game_world.add_object(common.boy, 1)
 
+    balls = [ Ball() for _ in range(100) ]
+    game_world.add_object(Ball(), 1)
 
 def finish():
     game_world.clear()
