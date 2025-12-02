037c3f71146bab909ad18d3c46056209764f0d5c
Gwangmin Song <thdalsrhkd@naver.com>
2025-12-02 17:35:02 +0900

    강의 내용

--- Lecture18_Scrolling/court.py
+++ Lecture18_Scrolling/court.py
@@ -8,13 +8,25 @@ class Court:
         self.ch = get_canvas_height()
         # fiil here
 
+        self.w = self.image.w
+        self.h = self.image.h
 
     def update(self):
         # fill here
+        self.window_left = clamp(0,int(common.boy.x) - self.cw // 2, self.w - self.cw - 1)
+        self.window_bottom = clamp(0,int(common.boy.y) - self.ch // 2, self.h - self.ch - 1)
+
         pass
 
     def draw(self):
-        self.image.draw(self.cw // 2, self.ch // 2)
+        self.image.clip_draw_to_origin(
+            self.window_left,
+            self.window_bottom,
+            self.cw,
+            self.ch,
+            0,
+            0
+        )
 
 
 class TileCourt:
@@ -23,7 +35,6 @@ class TileCourt:
         self.ch = get_canvas_height()
 
         # fill here
-
 
     def update(self):
         pass
